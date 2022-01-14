cc70a7bb850a231806b7ca635d8db5ec654ca0e2
kiingjo <bluesh5050@naver.com>
2022-01-14 17:09:31 +0900

    파이썬 1일차 진행

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Studypython
+빅데이터 반 파이썬 학습 리포지토리
+
+### 1일차
+- 프로그래밍 이해
+- 파이썬 설치, Visual studio code 설치
+- 개발환경 학습
+- 기본문법
+    - 콘솔출력 Hello World
+    - tkclrdustks
+    - type()함수
+    - int(), float() 형변환 함수
---
+++ day1/기본문법.py
@@ -0,0 +1,37 @@
+# 주석입니다
+# 문자열 표현방법 둘다 가능하다
+print('Hello,World')
+print("Hello,World")
+
+# 숫자 연산 출력
+# print()활용
+print(3 + 4)
+
+#변수사용
+a = "안녕하세요. "
+b = "반갑습니다!!!"
+print(a)
+print(b)
+print(a,b, "정말?")
+print(a+b, "정말?")
+
+# 수학연산
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 3)
+print(5 // 3) #몫만 계산
+print(5%3) #mod랑 같은 뜻
+print(2**10) # 거듭제곱
+
+print(int(5/3))
+print(int('10'))
+
+#type 확인
+print(type(10))     #정수일 때는 int
+print(type(10.0)) #소숫점 있을 때는 float
+
+#괄호 (연산자 우선순위)
+print((2 + 3) + 5)
+print(2 + (3 + 5))
+
